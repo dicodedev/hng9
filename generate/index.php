45cfef9956417fd9b740427105943aa8da2def07
chisainty <johnchinedudivine@gmail.com>
2022-11-02 22:50:32 +0100

    updated codes

--- generate/index.php
+++ generate/index.php
@@ -5,10 +5,18 @@ $newarray = array();
 
 $CSVfp = fopen($file, "r+");
 if ($CSVfp !== FALSE) {
+    $added_title = false;
     $count = 0;
+    $max_count = 22; //team name + 20 members
     while (!feof($CSVfp)) {
         $data = fgetcsv($CSVfp, 1000, ",");
-        if ($count && !empty($data)) {
+        // print_r($data);
+
+        //add the team name if the title has been added
+        if (!$count && $added_title) array_push($newarray, [$data[0], '', '', '', '', '']);
+
+        //add item if the title has been added and you are not on the gap line
+        if ($added_title && $count && ($count < ($max_count)) && !empty($data)) {
             //get values
             $serial_number = $data[0];
             $filename = $data[1];
@@ -19,17 +27,17 @@ if ($CSVfp !== FALSE) {
             $array = array(
                 'format' => 'CHIP-0007',
                 'name' => '',
-                'description' => '',
+                'description' => $des,
                 'minting_tool' => 'Team x',
                 'sensitive_content' => false,
-                'series_number' => '',
+                'series_number' => $serial_number,
                 'series_total' => 526,
                 'attributes' =>
                 array(
                     0 =>
                     array(
                         'trait_type' => 'gender',
-                        'value' => '',
+                        'value' => $gender,
                     ),
                 ),
                 'collection' =>
@@ -41,7 +49,7 @@ if ($CSVfp !== FALSE) {
                         0 =>
                         array(
                             'type' => 'description',
-                            'value' => 'Rewards for accomplishments during HNGi9.',
+                            'value' => $des,
                         ),
                     ),
                 ),
@@ -49,13 +57,32 @@ if ($CSVfp !== FALSE) {
             $json = json_encode($array);
 
             //hash json
-            $new_file_name = $filename . "." . hash('sha256', $json) . ".csv";
-            array_push($newarray, [str_replace('"', '', $data[0]), str_replace('"', '', $data[1]), str_replace('"', '', $data[2]), str_replace('"', '', $data[3]), str_replace('"', '', $data[4]), str_replace('"', '', $new_file_name)]);
+            $hash = hash('sha256', $json);
+            if (isset($data[1]) && !empty($data[1])) {
+                array_push($newarray, [str_replace('"', '', $data[0]), str_replace('"', '', $data[1]), str_replace('"', '', $data[2]), str_replace('"', '', $data[3]), str_replace('"', '', $data[4]), str_replace('"', '', $hash)]);
+            }else{
+                array_push($newarray, [str_replace('"', '', $data[0]), str_replace('"', '', $data[1]), str_replace('"', '', $data[2]), str_replace('"', '', $data[3]), str_replace('"', '', $data[4]), '']);
+            }
         }
-        if (!$count) {
-            array_push($newarray, [str_replace('"', '', $data[0]), str_replace('"', '', $data[1]), str_replace('"', '', $data[2]), str_replace('"', '', $data[3]), str_replace('"', '', $data[4]), str_replace('"', '', "sha256")]);
+
+        if ($added_title && !($count === ($max_count - 1))) {
             $count++;
-        };
+        }
+
+        //gotten to the gap, reset counter
+        if ($count === ($max_count)) {
+            $count = 0;
+        }
+
+
+
+
+        //add the file title
+        if (!$added_title) {
+            array_push($newarray, [str_replace('"', '', $data[0]), str_replace('"', '', $data[1]), str_replace('"', '', $data[2]), str_replace('"', '', $data[3]), str_replace('"', '', $data[4]), str_replace('"', '', "sha256")]);
+            // $count++;
+            $added_title = true;
+        }
     }
 
     // Open a file in write mode ('w')
@@ -67,4 +94,5 @@ if ($CSVfp !== FALSE) {
     fclose($fp);
 }
 fclose($CSVfp);
-echo "<h2>Done generating!!</h2><a download href='".$new_file_destination."'>Click here</a> to download csv file";
+//display 
+echo "<h2>Done generating!!</h2><a download href='" . $new_file_destination . "'>Click here</a> to download csv file";
