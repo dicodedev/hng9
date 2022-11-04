2e8cbeb09ac0fe607f57606680c115ef96ec89a1
chisainty <johnchinedudivine@gmail.com>
2022-11-04 11:50:25 +0100

    latest commit

---
+++ openapi.php
@@ -0,0 +1,11 @@
+<?php
+$answer = $open_ai->answer([
+    'documents' => ['Puppy A is happy.', 'Puppy B is sad.'],
+    'question' => 'which puppy is happy?',
+    'search_model' => 'ada',
+    'model' => 'curie',
+    'examples_context' => 'In 2017, U.S. life expectancy was 78.6 years.',
+    'examples' => [['What is human life expectancy in the United States?', '78 years.']],
+    'max_tokens' => 5,
+    'stop' => ["\n", '<|endoftext|>'],
+  ]);
