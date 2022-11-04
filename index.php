0a79ad1357b808053d4afdd6fb6c35d5c0e5811f
chisainty <johnchinedudivine@gmail.com>
2022-11-04 10:16:18 +0100

    latest commit

--- index.php
+++ index.php
@@ -2,13 +2,18 @@
 //HEADERS
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Allow-Origin");
 
 $headers = apache_request_headers();
 $res = array();
 
 $body = json_decode(file_get_contents("php://input"), true);
+
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+fwrite($myfile, file_get_contents("php://input"));
+fclose($myfile);
+
 if (isset($body['operation_type']) && isset($body['x']) && isset($body['y']) && gettype($body['x']) == 'integer' && gettype($body['y']) == 'integer') {
     // set response code - 200 OK
     http_response_code(200);
@@ -62,5 +67,5 @@ if (isset($body['operation_type']) && isset($body['x']) && isset($body['y']) && 
     $res["operation_type"] = $string;
     $res["result"] = $result;
 }
-
 echo json_encode($res);
+// echo "slackUsername=" . $res["slackUsername"] . "&operation_type=" . $string . "&result=" . $result;
